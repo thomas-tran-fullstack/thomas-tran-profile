69fe3f7a8578637e8e1032004171aab23bcee464
thomas-tran-fullstack <246865264+thomas-tran-fullstack@users.noreply.github.com>
2025-12-07 18:05:42 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ script.js
@@ -0,0 +1,6 @@
+const obs = new IntersectionObserver(entries => {
+  entries.forEach(e => {
+    if (e.isIntersecting) e.target.classList.add("show");
+  });
+});
+document.querySelectorAll(".fade").forEach(el => obs.observe(el));
